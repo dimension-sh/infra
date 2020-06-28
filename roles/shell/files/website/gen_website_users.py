83a43f9a37ca8af07a86302dc0b7fc7a145908d5
Andrew Williams <andy@tensixtyone.com>
2020-06-28 18:34:16 +0100

    Add website cron scripts

---
+++ roles/shell/files/website/gen_website_users.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Updates the user page on the website with the current user list
+
+import pwd
+import os.path
+
+users = 0
+with open('/var/www/dimension.sh/inc/users.gen.html', 'w') as fobj:
+  fobj.write("<ul>\n")
+  for user in pwd.getpwall():
+    if not os.path.exists(os.path.join(user.pw_dir, 'public_html')) or user.pw_gid != 100:
+      continue
+    gecos = ''
+    if user.pw_gecos:
+      gecos = ' - %s' % user.pw_gecos
+    entry = "<li><a href='/~%s/'>~%s</a>%s</li>\n" % (user.pw_name, user.pw_name, gecos)
+    users += 1
+    fobj.write(entry)
+  fobj.write("</ul>\n")
+  fobj.write("<p>Total Users: <b>%d</b></p>" % users)
