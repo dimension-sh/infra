7deed13ddfeb412bdacc307dd9b816109ab43900
Andrew Williams <andy@tensixtyone.com>
2020-06-29 21:07:00 +0100

    Skip users who opt out of the userlist

--- roles/shell/files/website/gen_website_users.py
+++ roles/shell/files/website/gen_website_users.py
@@ -8,8 +8,15 @@ users = 0
 with open('/var/www/dimension.sh/inc/users.gen.html', 'w') as fobj:
   fobj.write("<ul>\n")
   for user in pwd.getpwall():
+
+    # if the user doesn't have a public_html folder, skip
     if not os.path.exists(os.path.join(user.pw_dir, 'public_html')) or user.pw_gid != 100:
       continue
+
+    # if .hidden exists in the public_html, skip it
+    if os.path.exists(os.path.join(user.pw_dir, 'public_html', '.hidden')):
+      continue
+
     gecos = ''
     if user.pw_gecos:
       gecos = ' - %s' % user.pw_gecos
